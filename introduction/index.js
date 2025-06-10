369e2644979e63f06ae360912ca889af39b37bad
24b28 <161195329+24b28@users.noreply.github.com>
2025-06-10 19:10:10 +0800

    Delete introduction directory

--- introduction/index.js
+++
@@ -1,8 +0,0 @@
-//console.log(`Hello`);
-
-//window.alert(`This is an alert!`);
-
-document.getElementById("fucker").textContent = "hi!";
-document.getElementById("suckp").textContent = "this is my first website :3";
-
-let x
