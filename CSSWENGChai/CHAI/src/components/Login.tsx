32784bb45b82e842900bdd3a630870fd11c7081c
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-06-10 20:21:00 +0800

    Added toastify and basic validation for login. Route doesn't work as Profile doesnt exist

--- CSSWENGChai/CHAI/src/components/Login.tsx
+++ CSSWENGChai/CHAI/src/components/Login.tsx
@@ -1,28 +1,50 @@
 import CHAI from '../assets/CHAI.jpg'
 import '../css/styles.css'
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
 
 function Login(){
+
+    const navigate = useNavigate();
+
+    const login = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.target as HTMLFormElement);
+        console.log(formData.get("username"))
+
+        if(!formData.get("username") || !formData.get("pw")) {
+            
+            toast.error("Please input your username and password.");
+        }
+        else {
+            navigate("/profile")
+        }
+    }
+
     return (
         <>
             <img src= {CHAI} alt="Logo" className='w-45 h-45 p-1 mt-10 mb-10 m-auto rounded-full border-3 border-solid border-[#E3E3E3]'/>
             <h2 className=' font-[Montserrat] font-bold text-2xl flex justify-center'>Management and Events Tracker (MET)</h2>
             <div className='m-auto w-[60vh] p-10'>
-                <form className='flex flex-col'>
+                <form className='flex flex-col' onSubmit={login}>
                     <label htmlFor='username' className='font-[Montserrat] font-semibold'>Username</label>
                     <input 
-                        id='username' 
+                        id='username'
+                        name='username'  
                         type='text'
                         className='border-solid border-3 rounded-[5px] p-1.5'
                     />
                     <label htmlFor='password' className='font-[Montserrat] font-semibold mt-5'>Password</label>
                     <input 
-                        id='password'
+                        id='pw'
+                        name='pw'
                         type='password'
                         className=' border-solid border-3 rounded-[5px] p-1.5'
                     />
                     <div className='flex justify-between mt-1.5'>
                         <label className="font-[Montserrat] text-[1.1rem]">
-                            <input type="checkbox" className='mr-2 h-4 w-4'/>
+                            <input id='cbox' name='cbox' type="checkbox" className='mr-2 h-4 w-4'/>
                             Remember Me
                         </label>
                         <a href='http://google.com' target='_blank' className='font-[Montserrat] text-[1.1rem] text-[#45B29D] hover:underline'>Forgot Password?</a>
--- CSSWENGChai/CHAI/src/main.tsx
+++ CSSWENGChai/CHAI/src/main.tsx
@@ -1,12 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import {Bounce, ToastContainer } from "react-toastify";
 import './css/styles.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              transition={Bounce}
+        />
       <App />
     </BrowserRouter>
   </StrictMode>,
