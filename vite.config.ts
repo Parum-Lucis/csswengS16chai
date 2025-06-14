6134fdd49828ae8b4d705e39c41958db2577ca5e
Liana Ho <143952121+dlpho@users.noreply.github.com>
2025-06-14 21:23:27 +0800

    qr code for testing local (dev) run on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { qrcode } from 'vite-plugin-qrcode';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    qrcode() // only applies in dev mode
   ],
 })
