c33639940d5cdfbaf7bd87d5f89cf31b18f80528
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-06-30 22:22:30 +0800

    omg forgot these

--- src/routes/VolunteerProfile.tsx
+++ src/routes/VolunteerProfile.tsx
@@ -73,7 +73,7 @@ export function VolunteerProfile() {
     const handleSave = 
     async () => {
         setForm(!formState)
-        if(!sex || !contact || email || address)
+        if(!sex || !contact || !email || !address)
         return
         if(sex != "M" && sex != "F")
         return
@@ -107,7 +107,7 @@ export function VolunteerProfile() {
                             </button>
                             <button
                                 className="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-2 rounded"
-                                onClick={handleConfirm} // TODO: REPLACE
+                                onClick={handleConfirm}
                             >
                                 Confirm Delete
                             </button>
--- src/routes/YourProfile.tsx
+++ src/routes/YourProfile.tsx
@@ -107,7 +107,7 @@ export function YourProfile() {
                             </button>
                             <button
                                 className="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-2 rounded"
-                                onClick={handleConfirm} // TODO: REPLACE
+                                onClick={handleConfirm}
                             >
                                 Confirm Delete
                             </button>
