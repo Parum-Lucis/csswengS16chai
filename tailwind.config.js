045f92be4f5a58422e375104e80ecf36c89a3d8d
Chy <chynna_tria@dlsu.edu.ph>
2025-06-30 13:06:35 +0800

    Added files for theme scalability

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#3EA08D",
+        secondary: "#254151",
+        text_1: "#254151",
+        text_2: "#ffffff",
+      },
+    },
+  },
+};
