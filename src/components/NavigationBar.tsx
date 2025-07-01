ffdcb5c4ca4d601d9e0ea42c448eab89127b9b7a
RD-Tan <ross_tan@dlsu.edu.ph>
2025-07-01 22:29:35 +0800

    fixed navbar issue (no key) (unrelated to this branch)

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,31 +4,32 @@ import { useContext } from "react";
 import { UserContext } from "../context/userContext.ts";
 
 
-function NavigationBar(){
+function NavigationBar() {
     const user = useContext(UserContext)
     const urls = [
-        {  name: "Admin",  pldt: "/view-admin" },
-        {  name: "Search", pldt: "/view-profile-list"},
-        {  name: "You", pldt: "/view-profile" },
-        {  name: "Events", pldt: "/view-event-list"},
-        {  name: "Calendar", pldt: "/view-calendar" }
+        { name: "Admin", pldt: "/view-admin" },
+        { name: "Search", pldt: "/view-profile-list" },
+        { name: "You", pldt: "/view-profile" },
+        { name: "Events", pldt: "/view-event-list" },
+        { name: "Calendar", pldt: "/view-calendar" }
     ];
 
-    return(
+    return (
         <>
-            { user && (
-            <div className="flex items-center justify-center p-4 fixed bottom-0 left-0  bg-[#254151] font-[Montserrat] font-bold text-white w-full border-t border-t-gray-400 shadow-[0px_-3px_20px] shadow-gray-500">
-                <div className="flex flex-row gap-3 md:gap-10">
-                    { urls.map ((ur) => (
-                        <NavLink to={ur.pldt}
-                        className={({ isActive, isPending }) =>
-                                        (isPending ? "pending " : 
+            {user && (
+                <div className="flex items-center justify-center p-4 fixed bottom-0 left-0  bg-[#254151] font-[Montserrat] font-bold text-white w-full border-t border-t-gray-400 shadow-[0px_-3px_20px] shadow-gray-500">
+                    <div className="flex flex-row gap-3 md:gap-10">
+                        {urls.map((ur) => (
+                            <NavLink to={ur.pldt}
+                                key={ur.pldt}
+                                className={({ isActive, isPending }) =>
+                                    (isPending ? "pending " :
                                         isActive ? "text-amber-300 " : "")
                                     + "flex duration-100 hover:translate-y-[-5px] text-sm"}> {ur.name}</NavLink>
                         ))
-                    }
-                </div>
-            </div>)
+                        }
+                    </div>
+                </div>)
             }
         </>
     )
