05bcba3dbfa90eb41bb6b6ad6ffe562325ed06ec
RD-Tan <ross_tan@dlsu.edu.ph>
2025-07-03 15:05:56 +0800

    admin only access to volunteer creation and delete volunteer profile now possible

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,8 +15,9 @@ import * as logger from "firebase-functions/logger";
 
 import { Volunteer } from "@models/volunteerType";
 // import { generateRandomPassword } from "./util/generatePassword";
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore, Timestamp } from "firebase-admin/firestore";
 import { generateRandomPassword } from "./utils/generatePassword";
+import { onSchedule } from "firebase-functions/scheduler";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -34,12 +35,14 @@ import { generateRandomPassword } from "./utils/generatePassword";
 setGlobalOptions({ maxInstances: 10 });
 
 
-
 const app = initializeApp();
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 
 export const createVolunteerProfile = onCall<Volunteer>(async (req) => {
+    if (!req.auth) return false;
+    if (!req.auth.token.is_admin) return false;
+
     const { email, is_admin } = req.data;
     try {
 
@@ -60,4 +63,37 @@ export const createVolunteerProfile = onCall<Volunteer>(async (req) => {
         return false;
     }
 
+})
+
+export const deleteVolunteerProfile = onCall<string>(async (req) => {
+    if (!req.auth) return false;
+
+    const uid = req.data;
+    try {
+
+        await firestore.doc(`volunteers/${uid}`).update({ time_to_live: 1000 * 60 * 60 * 24 * 30 })
+        return true;
+
+    } catch (error) {
+        logger.error(error)
+        return false;
+    }
+})
+
+export const cronCleaner = onSchedule("every 1 minutes", async () => {
+    try {
+        const snapshot = await firestore.collection("beneficiaries").where("time_to_live", "<=", Timestamp.now()).get();
+
+        const batch = firestore.batch();
+        snapshot.forEach(doc => {
+            logger.info(`Deleting ${doc.id}`);
+            batch.delete(doc.ref);
+        })
+
+        await batch.commit();
+        logger.info("Successfully deleted a bunch of guys! Count: " + snapshot.size)
+
+    } catch (error) {
+        logger.error(error)
+    }
 })
