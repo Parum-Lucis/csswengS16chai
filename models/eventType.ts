248d67a2eaab008055b746d7d397a1270959b353
Liana Ho <143952121+dlpho@users.noreply.github.com>
2025-07-09 21:55:30 +0800

    type Date > Timestamp

--- models/eventType.ts
+++ models/eventType.ts
@@ -1,10 +1,11 @@
+import type { Timestamp } from "firebase/firestore";
 import type { AttendedEvents } from "./attendedEventsType";
 
 export interface Event {
   docID: string;
   event_name: string;
   description: string;
-  start_date: Date;
-  end_date: Date;
+  start_date: Timestamp;
+  end_date: Timestamp;
   attendees: AttendedEvents[];
 }
