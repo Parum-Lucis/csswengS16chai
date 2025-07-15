b4663a4b26b83faa49c95f0d76fe1d485534074a
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-07-16 01:10:44 +0800

    fixed remove attendees, also more tech debt (attendees cannot be sorted by name without a lot of overhaul,,,,,)

--- src/components/AttendeesCard.tsx
+++ src/components/AttendeesCard.tsx
@@ -1,4 +1,4 @@
-function AttendeesCard({name, who_attended, attendance,}: {name: string; who_attended: string; attendance: boolean}) {
+function AttendeesCard({name, who_attended, attendance, handleToggle}: {name: string; who_attended: string; attendance: boolean, handleToggle: () => void}) {
     return ( // Add the return statement here
         <div className="w-full max-w-2xl flex flex-row items-center mt-2 h-[6vh] text-[1rem] mr-2 font-sans bg-primary text-white p-1.5 rounded-[5px] font-semibold mb-2">
           <div className="flex justify-center items-center bg-secondary mr-2 text-white p-2 rounded-[5px] w-[15vh] h-[4vh] font-semibold">
@@ -9,6 +9,7 @@ function AttendeesCard({name, who_attended, attendance,}: {name: string; who_att
             <input
               type="checkbox"
               className="form-checkbox h-5 w-5 rounded text-white bg-white border-white checked:accent-secondary checked:border-white ml-auto mr-3"
+              onChange={handleToggle}
             />
           </div>
         </div>
