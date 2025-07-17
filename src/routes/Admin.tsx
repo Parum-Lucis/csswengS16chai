9eadf18801f9a67f7ae3f0ffc69f85554c3a78c6
RD-Tan <ross_tan@dlsu.edu.ph>
2025-07-17 22:26:08 +0800

    fixed some urls

--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -10,9 +10,9 @@ function Admin() {
 
     const urls = [
         { name: "Create Beneficiary Profile", pldt: "/create-beneficiary-profile" },
-        { name: "View Deleted Beneficiaries", pldt: "/deleted-beneficiaries" },
-        { name: "Create Volunteer Profile", pldt: "volunteer/add" },
+        { name: "View Deleted Beneficiaries", pldt: "deleted-beneficiaries" },
         { name: "View Volunteers", pldt: "volunteer" },
+        { name: "Create Volunteer Profile", pldt: "volunteer/new" },
         { name: "View Deleted Volunteers", pldt: "volunteer/deleted" },
     ];
 
@@ -25,15 +25,13 @@ function Admin() {
 
     return (
         <div className="w-full min-h-screen flex bg-[#254151] items-start">
-            {(
-                <div className="flex wrap-anywhere p-9 gap-4 font-[Montserrat] font-bold w-full max-w-5xl text-white">
-                    {urls.map((ur) => (
-                        <NavLink to={ur.pldt} key={ur.name}
-                            className="flex duration-100 bg-[#45B29D] rounded-md text-2xl text-center p-6 hover:bg-[#45b29c8a]"> {ur.name}</NavLink>
-                    ))
-                    }
-                </div>)
-            }
+            <div className="flex flex-col wrap-anywhere p-9 gap-4 font-[Montserrat] font-bold w-full max-w-5xl text-white">
+                {urls.map((ur) => (
+                    <NavLink to={ur.pldt} key={ur.name}
+                        className="flex duration-100 bg-[#45B29D] rounded-md text-2xl text-center p-6 hover:bg-[#45b29c8a]"> {ur.name}</NavLink>
+                ))
+                }
+            </div>
         </div>
     )
 }
