de416ede4498e1750ec6cc5c80d78210b9c973e2
Sky282128 <hsualdrin28@gmail.com>
2025-07-18 13:53:02 +0800

    edited EventCard.tsx and made tests for it

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,4 +1,9 @@
-function EventCard({}: {}) {
+type EventCardProps = {
+  date: string;
+  event: string;
+};
+
+function EventCard({ date, event }: EventCardProps) {
   return (
     <div className="flex flex-row items-center mt-2 h-[6vh] text-[1rem] mr-2 font-sans bg-primary text-white p-1.5 rounded-[5px] font-semibold">
       <div className="flex justify-center items-center bg-secondary mr-2 text-white p-2 rounded-[5px] w-[15vh] h-[4vh] font-semibold">
