710be005c961c20577b5d6764f20cba344444c55
RD-Tan <ross_tan@dlsu.edu.ph>
2025-07-18 11:54:44 +0800

    naligaw

--- src/routes/YourProfile.tsx
+++ src/routes/YourProfile.tsx
@@ -9,6 +9,7 @@ import { createPortal } from 'react-dom';
 import { toast } from "react-toastify";
 import { callDeleteVolunteerProfile } from "../firebase/cloudFunctions";
 import { signOut } from "firebase/auth";
+import { emailRegex } from "../util/emailRegex.ts";
 
 export function YourProfile() {
 
@@ -97,10 +98,8 @@ export function YourProfile() {
                 toast.error("Please fill up all fields!")
                 return
             }
-            const emailRegEx = new RegExp(
-                /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-            ); // from https://emailregex.com/
-            if (!emailRegEx.test(email!)) {
+
+            if (!emailRegex.test(email!)) {
                 toast.error("Please input a proper email!");
                 return
             }
