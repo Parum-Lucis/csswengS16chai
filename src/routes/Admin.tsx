a43dbe3d2dace5c35b1e43dace84bfad8c145afe
RD-Tan <ross_tan@dlsu.edu.ph>
2025-07-19 13:17:19 +0800

    added admin navigation

--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -9,9 +9,11 @@ function Admin() {
     const user = useContext(UserContext)
 
     const urls = [
-        { name: "Create Volunteer Profile", pldt: "/create-volunteer-profile" },
         { name: "Create Beneficiary Profile", pldt: "/create-beneficiary-profile" },
         { name: "Create Event", pldt: "/create-event" },
+        { name: "View Volunteers", pldt: "volunteer/" },
+        { name: "Create Volunteer Profile", pldt: "volunteer/new" },
+        { name: "Deleted Volunteers", pldt: "volunteer/deleted" },
     ];
 
     useEffect(() => {
@@ -22,11 +24,11 @@ function Admin() {
     }, [user, navigate]);
 
     return (
-        <div className="w-full min-h-screen flex bg-secondary items-start">
-            <div className="flex flex-col wrap-anywhere p-9 gap-4 font-sans font-bold w-full max-w-5xl text-white">
+        <div className="w-full min-h-screen flex bg-secondary items-start justify-center">
+            <div className="grid md:grid md:grid-cols-3 auto-rows-fr wrap-anywhere p-9 gap-4 font-sans font-bold w-full max-w-5xl text-white">
                 {urls.map((ur) => (
                     <NavLink to={ur.pldt} key={ur.name}
-                        className="flex duration-100 bg-primary rounded-md text-2xl text-center p-6 hover:bg-onhover"> {ur.name}</NavLink>
+                        className="flex justify-center items-center duration-100 bg-primary rounded-md text-2xl text-center p-6 hover:bg-onhover"> {ur.name}</NavLink>
                 ))
                 }
             </div>
