09fe2cc6b8d17f57faca8e1bae362fd4d54246c4
Liana Ho <143952121+dlpho@users.noreply.github.com>
2025-07-25 22:14:59 +0800

    change to db entity models

--- models/volunteerType.ts
+++ models/volunteerType.ts
@@ -1,7 +1,7 @@
 import type { Timestamp } from "firebase/firestore";
 
 export interface Volunteer {
-  docID: string;
+  docID?: string;
   last_name: string;
   first_name: string;
   contact_number: string;
