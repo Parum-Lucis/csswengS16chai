477305a061854762d567a9b08ce58b64aa2840ee
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-07-31 02:02:51 +0800

    cleanup

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,6 +1,5 @@
 import type { AttendedEvents } from "@models/attendedEventsType";
 import { formatDate } from "date-fns";
-import { UsersRound, Baby, UserRound } from 'lucide-react';
 import { Link } from "react-router";
 
 function EventCard({attEvent}: {attEvent: AttendedEvents}) {
@@ -34,21 +33,11 @@ function EventCard({attEvent}: {attEvent: AttendedEvents}) {
         <div className="grow min-w-0">
           <div className="text-base font-bold">{attEvent.event_name}</div>
           <div>
-            <div className="text-sm">Date: {formatDate(new Date((attEvent.event_start.seconds ?? 0)*1000), "MM/d/yy")}<br />Time: {formatDate(new Date((attEvent.event_start.seconds ?? 0)*1000), "h:mm aa")}</div>
+            <div className="text-sm">Date: {formatDate(new Date((attEvent.event_start.seconds ?? 0)*1000), "MM/dd/yy")}<br />Time: {formatDate(new Date((attEvent.event_start.seconds ?? 0)*1000), "h:mm aa")}</div>
             <div className="text-sm">Attendee: {attEvent.who_attended}</div>
             <div className="text-sm">Attendance: {attendance}</div>
           </div>
       </div>
-{/* import { formatDate } from "date-fns";
-import type { Timestamp } from "firebase/firestore";
-
-function EventCard({ date, name }: { date: Timestamp, name: string }) {
-  return (
-    <div className="flex flex-row items-center mt-2 h-[6vh] text-[1rem] mr-2 font-sans bg-primary text-white p-1.5 rounded-[5px] font-semibold">
-      <div className="flex justify-center items-center bg-secondary mr-2 text-white p-2 rounded-[5px] w-[15vh] h-[4vh] font-semibold">
-        <h4>{formatDate(date.toDate(), "mm d, yyyy")}</h4>
-      </div>
-      <h3 className="flex">{name}</h3> */}
     </Link>
   );
 }
